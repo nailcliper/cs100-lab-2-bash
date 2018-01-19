31e96ce59f28cf324b700f46a95bd92fef0e9191
Jonathan Oaks <joaks001@ucr.edu>
2018-01-18 19:05:46 -0800

    added 'createEmptyMainFile' shell script, NameID txt, and the resulting 'main.cpp'

---
+++ createEmptyMainFile.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+touch main.cc
+cat Jonathan861303298.txt > main.cc
+echo "" >> main.cc
+echo "int main(int argc, const char** argv)" >> main.cc
+echo "{ }" >> main.cc
---
+++ main.cc
@@ -0,0 +1,6 @@
+Jonathan Oaks
+SID: 861303298
+Email: joaks001@ucr.edu
+
+int main(int argc, const char** argv)
+{ }
